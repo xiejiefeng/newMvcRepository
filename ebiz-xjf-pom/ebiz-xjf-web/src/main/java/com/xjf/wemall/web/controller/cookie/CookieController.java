c3faf1fcb34e4c2d81c5c3af6f316587a82e571b
xiejiefeng <xiejiefeng333@hotmail.com>
2016-01-07 17:19:15 +0800

    sui

---
+++ ebiz-xjf-pom/ebiz-xjf-web/src/main/java/com/xjf/wemall/web/controller/cookie/CookieController.java
@@ -0,0 +1,390 @@
+/*
+ * Copyright (C), 2013-2014, 上海汽车集团股份有限公司
+ * FileName: CookieController.java
+ * Author:   baowenzhou
+ * Date:     2015年07月01日 下午4:39:25
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package com.xjf.wemall.web.controller.cookie;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.saic.wemall.api.constant.WemallConstants;
+import com.saic.wemall.api.entity.common.AjaxObject;
+import com.xjf.wemall.api.entity.common.CookieObject;
+import com.saic.wemall.service.mail.api.MailService;
+import com.saic.wemall.service.menu.api.WXMenuService;
+import com.saic.wemall.service.redis.api.RedisJobService;
+import com.saic.wemall.service.shoppingcart.api.ShoppingCartService;
+import com.saic.wemall.service.sysreference.api.SysReferenceService;
+import com.saic.wemall.api.entity.mail.MailVo;
+import com.saic.wemall.api.entity.sysreference.SysReferenceDetailRestFulVo;
+import com.saic.wemall.api.entity.user.CustomCxbInfoVo;
+import com.saic.wemall.api.entity.user.PointVo;
+import com.saic.wemall.api.util.Base64Operate;
+import com.saic.wemall.api.util.JSONParser;
+import com.saic.wemall.service.wechat.api.UserInfoService;
+import com.xjf.wemall.web.controller.BaseController;
+import com.saic.wemall.web.util.CookieUtil;
+
+/**
+ * Cookie页面<br> 
+ * 〈功能详细描述〉
+ *
+ * @author baowenzhou
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+@Controller
+@RequestMapping("/cookie")
+public class CookieController extends BaseController{
+    /***
+     * 首页
+     */
+    private static final String COOKIE_FTL="/cookie/cookie.ftl";
+    
+    /***
+     * 更新缓存
+     */
+    private static final String RESET_FTL="/cookie/reset.ftl";
+    
+    /***
+     *     
+     * 功能描述:跳转关于我们 <br>
+     * 〈功能详细描述〉
+     *
+     * @return
+     * @see [相关类/方法](可选)
+     * @since [产品/模块版本](可选)
+     */
+    @RequestMapping("/menu")
+    public String index(HttpServletRequest request, Model model, HttpServletResponse response){
+    	
+    	// 获取Cookie对象
+    	CookieObject cookie = super.getCookie(response);
+    	
+    	//判断cookie中是否有openid
+    	model.addAttribute("openId", cookie.getOpenId());
+    	model.addAttribute("openType", cookie.getOpenType());
+    	model.addAttribute("cxId", cookie.getCxId());
+    	model.addAttribute("x", cookie.getLongitude());
+    	model.addAttribute("y", cookie.getLatitude());
+    	model.addAttribute("key", cookie.getKey());
+    	
+    	SysReferenceDetailRestFulVo queryVo = new SysReferenceDetailRestFulVo();
+    	queryVo.setPkReferenceRule(WemallConstants.REFERENCE_CODE_OPENTYPE);
+    	List<SysReferenceDetailRestFulVo> list = sysReferenceService.getReferenceRedis(queryVo);
+    	Collections.sort(list, new Comparator<SysReferenceDetailRestFulVo>(){
+			@Override
+			public int compare(SysReferenceDetailRestFulVo o1, SysReferenceDetailRestFulVo o2) {
+				return o1.getReferenceName().compareTo(o2.getReferenceName());
+			}
+    	});
+    	model.addAttribute("openTypeList", list);
+        return COOKIE_FTL;
+    }
+    
+//    /***
+//     *     
+//     * 功能描述:跳转关于我们 <br>
+//     * 〈功能详细描述〉
+//     *
+//     * @return
+//     * @see [相关类/方法](可选)
+//     * @since [产品/模块版本](可选)
+//     */
+//    @RequestMapping("/test")
+//    public String test(HttpServletRequest request, Model model){
+//    	//判断cookie中是否有openid
+//    	model.addAttribute("openId", this.getOpenId());
+//    	model.addAttribute("openType", this.getOpenType());
+//    	model.addAttribute("x", this.getLongitude());
+//    	model.addAttribute("y", this.getLatitude());
+//    	model.addAttribute("wx", openTypeWx);
+//    	model.addAttribute("cxb", openTypeCxb);
+//        return COOKIE_TEST_FTL;
+//    }
+    
+    /***
+	 * 
+	 * 功能描述: 设定OpenId<br>
+	 * 〈功能详细描述〉
+	 * 
+	 * @return
+	 * @see [相关类/方法](可选)
+	 * @since [产品/模块版本](可选)
+	 */
+	@RequestMapping(value = "/setCookie", method = RequestMethod.GET)
+	@ResponseBody
+	public AjaxObject setCookie(String openId, String openType, String cxId, String x, String y, String key,
+			HttpServletRequest request, HttpServletResponse response) {
+
+		AjaxObject ajaxObject = new AjaxObject();
+		
+		openId = openId.trim();
+		openType = openType.trim();
+		cxId = cxId.trim();
+		x = x.trim();
+		y = y.trim();
+
+		CookieUtil.addCookieMaxAge(response, CookieUtil.OPENID, openId);
+		CookieUtil.addCookieMaxAge(response, CookieUtil.OPEN_TYPE, openType);
+		CookieUtil.addCookieMaxAge(response, CookieUtil.CXID, cxId);
+		CookieUtil.addCookieMaxAge(response, CookieUtil.LOCATION_LONGITUDE, x);
+		CookieUtil.addCookieMaxAge(response, CookieUtil.LOCATION_LATITUDE, y);
+		CookieUtil.addCookieMaxAge(response, CookieUtil.USER_KEY, key);
+		
+		if (StringUtils.isNotEmpty(x) && StringUtils.isNotEmpty(y)) {
+			PointVo point = new PointVo();
+			if (sysRefFieldService.openTypeCxj().equals(openType)) {
+				point.setUserId(openId);
+			} else {
+				point.setUserId(cxId);
+			}
+			
+			point.setLongitude(Double.valueOf(x));
+			point.setLatitude(Double.valueOf(y));
+			userInfoService.putLocationToCache(key, point);
+		}
+
+		return ajaxObject;
+	}
+	
+	/***
+	 * 
+	 * 功能描述: 设定OpenId<br>
+	 * 〈功能详细描述〉
+	 * 
+	 * @return
+	 * @see [相关类/方法](可选)
+	 * @since [产品/模块版本](可选)
+	 */
+	@RequestMapping(value = "/createMenu", method = RequestMethod.GET)
+	@ResponseBody
+	public AjaxObject createMenu() {
+
+		AjaxObject ajaxObject = new AjaxObject();
+		
+		String resultStr = wxMenuService.createMenuForWX();
+		if (!resultStr.contains("\"errcode\":0")) {
+			ajaxObject.setResult(AjaxObject.FAILD);
+		}
+
+		return ajaxObject;
+	}
+	
+	/***
+	 * 
+	 * 功能描述: 设定OpenId<br>
+	 * 〈功能详细描述〉
+	 * 
+	 * @return
+	 * @see [相关类/方法](可选)
+	 * @since [产品/模块版本](可选)
+	 */
+	@RequestMapping(value = "/resetRedis", method = RequestMethod.GET)
+	@ResponseBody
+	public AjaxObject resetRedis(String index, HttpServletResponse response) {
+
+		AjaxObject ajaxObject = new AjaxObject();
+		
+		// 获取Cookie对象
+    	CookieObject cookie = super.getCookie(response);
+		String key = cookie.getKey();
+		
+		boolean result = true;
+		
+		switch(index) {
+		case "shopping": // 刷新购物车缓存
+			shoppingCartService.remove(key);
+			break;
+		case "mail": // 刷新邮件服务
+			result = mailService.init();
+			break;
+		default:
+			result = redisJobService.resetRedis(index);
+			break;
+		}
+
+		if (!result) {
+			ajaxObject.setResult(AjaxObject.FAILD);
+			ajaxObject.setMsg("重置失败!");
+		}
+		
+		return ajaxObject;
+	}
+	
+	/***
+	 * 
+	 * 功能描述: 获取不同场景参数（Base64）<br>
+	 * 〈功能详细描述〉
+	 * 
+	 * @return
+	 * @see [相关类/方法](可选)
+	 * @since [产品/模块版本](可选)
+	 */
+	@RequestMapping(value = "/getParam", method = RequestMethod.GET)
+	@ResponseBody
+	public AjaxObject getParam(CustomCxbInfoVo customCxbInfo, HttpServletResponse response) {
+		
+		AjaxObject ajaxObject = new AjaxObject();
+		
+		String json = JSONParser.toJSONString(customCxbInfo);
+		json = Base64Operate.toBASE64(json);
+		json = json.replaceAll("\r\n", "");
+		ajaxObject.setObj(json);
+		
+		return ajaxObject;
+	}
+	
+	/***
+	 * 
+	 * 功能描述: 更新缓存信息<br>
+	 * 〈功能详细描述〉
+	 * 
+	 * @return
+	 * @see [相关类/方法](可选)
+	 * @since [产品/模块版本](可选)
+	 */
+	@RequestMapping(value = "/reset", method = RequestMethod.GET)
+	public String reset() {
+		return RESET_FTL;
+	}
+	
+	
+	/***
+	 * 
+	 * 功能描述: 更新缓存信息<br>
+	 * 〈功能详细描述〉
+	 * 
+	 * @return
+	 * @see [相关类/方法](可选)
+	 * @since [产品/模块版本](可选)
+	 */
+	@RequestMapping(value = "/reset_{index}", method = RequestMethod.GET)
+	@ResponseBody
+	public String resetRedis(@PathVariable String index) {
+		
+		boolean result = false;
+		String name = "";
+		
+		switch(index) {
+		case "store": // 刷新门店相关缓存
+			name = "门店相关";
+			break;
+		case "product": // 刷新服务相关缓存
+			name = "服务相关";
+			break;
+		case "carModel": // 刷新车型相关缓存
+			name = "车型相关";
+			break;
+		case "reference": // 刷新业务参照缓存
+			name = "业务参照";
+			break;
+		case "maintenance": // 刷新保养手册缓存
+			name = "保养手册";
+			break;
+		case "artificer": // 刷新技师相关缓存
+			name = "技师相关";
+			break;
+		case "sysReference": // 刷新系统参照缓存
+			name = "系统参照";
+			break;
+		}
+		
+		result = redisJobService.resetRedis(index);
+		
+		if (result) {
+			return name + "更新成功";
+		} else {
+			return name + "更新失败";
+		}
+	}
+	
+	/***
+	 * 
+	 * 功能描述: 获取Redis<br>
+	 * 〈功能详细描述〉
+	 * 
+	 * @return
+	 * @see [相关类/方法](可选)
+	 * @since [产品/模块版本](可选)
+	 */
+	@RequestMapping(value = "/getRedis", method = RequestMethod.GET)
+	@ResponseBody
+	public AjaxObject getRedis(String key, HttpServletResponse response) {
+		
+		AjaxObject ajaxObject = new AjaxObject();
+		
+		boolean exists = redisClient.exists(key);
+		
+		if (!exists) {
+			ajaxObject.setResult(AjaxObject.FAILD);
+			return ajaxObject;
+		}
+		
+		String type = redisClient.type(key).toUpperCase();
+		String value = "";
+
+		if ("STRING".equals(type)) {
+			value = redisClient.get(key);
+		} else if("HASH".equals(type)) {
+			value = JSONParser.toJSONString(redisClient.hgetAll(key));
+			value = value.replaceAll("\"\\[", "\\[");
+			value = value.replaceAll("\\]\"", "\\]");
+			value = value.replaceAll("\"\\{", "\\{");
+			value = value.replaceAll("\\}\"", "\\}");
+			value = value.replaceAll("\\\\\"", "\"");
+		}
+		
+		if (StringUtils.isEmpty(value)) {
+			ajaxObject.setResult(AjaxObject.FAILD);
+		} else {
+			ajaxObject.setObj(value);
+		}
+		
+		return ajaxObject;
+	}
+	
+	/***
+	 * 
+	 * 功能描述: 获取Redis<br>
+	 * 〈功能详细描述〉
+	 * 
+	 * @return
+	 * @see [相关类/方法](可选)
+	 * @since [产品/模块版本](可选)
+	 */
+	@RequestMapping(value = "/sendMail", method = RequestMethod.GET)
+	@ResponseBody
+	public AjaxObject sendMail(MailVo mail, HttpServletResponse response) {
+		
+		AjaxObject ajaxObject = new AjaxObject();
+		
+		boolean ret = mailService.send(mail);
+		
+		if (!ret) {
+			ajaxObject.setResult(AjaxObject.FAILD);
+		}
+		
+		return ajaxObject;
+	}
+}
---
+++ ebiz-xjf-pom/ebiz-xjf-web/src/main/java/com/xjf/wemall/web/util/CookieUtil.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright (C), 2013-2014, 上海汽车集团股份有限公司
+ * FileName: CookieUtil.java
+ * Author:   wanglijun
+ * Date:     2014年8月29日 上午9:52:45
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package com.xjf.wemall.web.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Cookie 工具类<br>
+ * 〈功能详细描述〉
+ * 
+ * @author wanglijun
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class CookieUtil {
+	
+//	/**日志类*/
+//	private static final Logger logger = LoggerFactory.getLogger(CookieUtil.class);
+    
+    /**OPENID*/
+    public static final String OPENID = "wemall_openid";
+    
+    /**OPEN_TYPE*/
+    public static final String OPEN_TYPE = "wemall_opentype";
+    
+    /**车享ID*/
+    public static final String CXID = "wemall_cxid";
+    
+    /**用户标识*/
+    public static final String USER_KEY = "wemall_userkey";
+    
+    /**jsessionid*/
+    public static final String JSESSIONID = "wemall_jsessionid=";
+    
+    /**location 经度*/
+    public static final String LOCATION_LONGITUDE = "wemall_locationlongitude";
+    
+    /**location 纬度*/
+    public static final String LOCATION_LATITUDE = "wemall_locationlatitude";
+    
+    /**SHOPPINGCATID*/
+    public static final String SHOPPINGCARTID = "wemall_shoppingcartid";
+    
+    /**跳转页面*/
+    public static final String TARGET_URL = "wemall_targeturl";
+    
+    /**OPENID 有效时间*/
+    public static final int OPENID_MAXAGE = 365*24*3600;
+    
+//    /** 二进制的redis. */
+//	@Autowired
+//	@Qualifier("springBinaryRedisClient")
+//	private IBinaryRedisClient springBinaryRedisClient;
+	
+//	private static CookieUtil cookieUtil;
+	
+//	public void setSpringBinaryRedisClient(IBinaryRedisClient springBinaryRedisClient){
+//		this.springBinaryRedisClient = springBinaryRedisClient;
+//	}
+	
+    public void init()
+    {
+//		cookieUtil = this;
+//		cookieUtil.springBinaryRedisClient = this.springBinaryRedisClient;
+    }
+    
+    /**
+     * 设置cookie
+     * 
+     * @param response
+     * @param name cookie名字
+     * @param value cookie值
+     * @param maxAge cookie生命周期 以秒为单位
+     */
+    public static void addCookieMaxAge(HttpServletResponse response, String name, String value) {
+    	CookieUtil.addCookie(null, response, name, value, OPENID_MAXAGE);
+    }
+
+    /**
+     * 设置cookie
+     * 
+     * @param response
+     * @param name cookie名字
+     * @param value cookie值
+     * @param maxAge cookie生命周期 以秒为单位
+     */
+    public static void addCookie(HttpServletRequest request, HttpServletResponse response, 
+    		String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        if (maxAge > 0)
+            cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+        
+//        String sessionId = request.getSession().getId();
+//        
+//        SessionObject session = cookieUtil.springBinaryRedisClient.get(
+//        		AbstractService.USERINFO_SESSION_KEY + sessionId, 
+//        		new TypeReference<SessionObject>() {});
+//        
+//        if (session == null) {
+//        	session = new SessionObject();
+//        }
+//        
+//        switch (name) {
+//        case CookieUtil.OPENID:
+//        	session.setOpenId(value);
+//        	break;
+//        case CookieUtil.OPEN_TYPE:
+//        	session.setOpenType(value);
+//        	break;
+//        case CookieUtil.LOCATION_LONGITUDE:
+//        	session.setLongitude(value);
+//        	break;
+//        case CookieUtil.LOCATION_LATITUDE:
+//        	session.setLatitude(value);
+//        	break;
+//        }
+//        
+//        cookieUtil.springBinaryRedisClient.setex(
+//        		AbstractService.USERINFO_SESSION_KEY + sessionId, 
+//        		session, TimeToLive.DAY.code());
+//        logger.info("session:set to redis:{}", sessionId);
+    }
+
+    /***
+     * 
+     * 功能描述:根据Cookie Name 获取<br>
+     * 〈功能详细描述〉
+     * 
+     * @param request
+     * @param name
+     * @return
+     * @see [相关类/方法](可选)
+     * @since [产品/模块版本](可选)
+     */
+    public static String getCookieByName(HttpServletRequest request, String name) {
+        Map<String, Cookie> cookieMap = ReadCookieMap(request);
+        if (cookieMap.containsKey(name)) {
+            Cookie cookie = (Cookie) cookieMap.get(name);
+            if (cookie != null) {
+            	return cookie.getValue();
+            }
+        }
+        
+//        //String sessionId = request.getSession().getId();
+//        String requestUri = request.getRequestURI();
+//        int idx = requestUri.indexOf(JSESSIONID);
+//        if (idx == -1) {
+//        	return null;
+//        }
+//        String sessionId = requestUri.substring(idx+JSESSIONID.length());
+//        
+//        SessionObject session = cookieUtil.springBinaryRedisClient.get(
+//        		AbstractService.USERINFO_SESSION_KEY + sessionId, 
+//        		new TypeReference<SessionObject>() {});
+//        
+//        logger.info("session:get from redis:{}", sessionId);
+//        
+//        if (session != null) {
+//        	switch (name) {
+//            case CookieUtil.OPENID:
+//            	return session.getOpenId();
+//            case CookieUtil.OPEN_TYPE:
+//            	return session.getOpenType();
+//            case CookieUtil.LOCATION_LONGITUDE:
+//            	return session.getLongitude();
+//            case CookieUtil.LOCATION_LATITUDE:
+//            	return session.getLatitude();
+//            }
+//        }
+        
+        return null;
+    }
+
+    /***
+     * 
+     * 功能描述: 读取cookie数据<br>
+     * 〈功能详细描述〉
+     * 
+     * @param request
+     * @return
+     * @see [相关类/方法](可选)
+     * @since [产品/模块版本](可选)
+     */
+    private static Map<String, Cookie> ReadCookieMap(HttpServletRequest request) {
+        Map<String, Cookie> cookieMap = new HashMap<String, Cookie>();
+        Cookie[] cookies = request.getCookies();
+        if (null != cookies) {
+            for (Cookie cookie : cookies) {
+                cookieMap.put(cookie.getName(), cookie);
+            }
+        }
+        return cookieMap;
+    }
+}

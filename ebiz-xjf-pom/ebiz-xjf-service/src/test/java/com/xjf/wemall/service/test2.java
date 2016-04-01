ef1d894c5385e8276e4ad6e13ea3258afe59987b
xiejiefeng <xiejiefeng333@hotmail.com>
2016-04-01 17:16:06 +0800

    loop for

--- ebiz-xjf-pom/ebiz-xjf-service/src/test/java/com/xjf/wemall/service/test2.java
+++ ebiz-xjf-pom/ebiz-xjf-service/src/test/java/com/xjf/wemall/service/test2.java
@@ -17,6 +17,28 @@ public class test2 {
 		System.out.println(new DecimalFormat("#.00").format(new Double(aa)));
 		System.out.println(new DecimalFormat("#.00").format(new Double(bb)));
 		System.out.println(new DecimalFormat("#.00").format(new Double(CC)));
+
+		loop: for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (i == 3 && j == 4) {
+					System.out.println();
+					continue loop;
+				}
+				System.out.print(i + "*" + j + "=" + i * j + "    ");
+			}
+			System.out.println();
+		}
+		
+		loop: for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (i == 3 && j == 4) {
+					System.out.println();
+					break loop;
+				}
+				System.out.print(i + "*" + j + "=" + i * j + "    ");
+			}
+			System.out.println();
+		}
 	}
 
 }

120cccaf2d0f76938b92e8b5d7b7b93b342abf41
xiejiefeng <xiejiefeng333@hotmail.com>
2016-07-15 18:03:15 +0800

    ///

--- ebiz-xjf-pom/ebiz-xjf-web/src/main/java/com/xjf/wemall/web/controller/cookie/CookieController.java
+++ ebiz-xjf-pom/ebiz-xjf-web/src/main/java/com/xjf/wemall/web/controller/cookie/CookieController.java
@@ -10,8 +10,11 @@
  */
 package com.xjf.wemall.web.controller.cookie;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -729,5 +732,65 @@ public class CookieController extends BaseController{
 		}
         
         return null;
+	}
+	
+	/**
+	 * 获得主机IP
+	 *
+	 * @return String
+	 */
+	private boolean isWindowsOS() {
+		boolean isWindowsOS = false;
+		String osName = System.getProperty("os.name");
+		if (osName.toLowerCase().indexOf("windows") > -1) {
+			isWindowsOS = true;
+		}
+		return isWindowsOS;
+	}
+	/**
+	 * 获取本机ip地址，并自动区分Windows还是linux操作系统
+	 * 
+	 * @return String
+	 */
+	private String getLocalIP() {
+		String sIP = "";
+		InetAddress ip = null;
+		try {
+			// 如果是Windows操作系统
+			if (isWindowsOS()) {
+				ip = InetAddress.getLocalHost();
+			}
+			// 如果是Linux操作系统
+			else {
+				boolean bFindIP = false;
+				Enumeration<NetworkInterface> netInterfaces = (Enumeration<NetworkInterface>) NetworkInterface
+						.getNetworkInterfaces();
+				while (netInterfaces.hasMoreElements()) {
+					if (bFindIP) {
+						break;
+					}
+					NetworkInterface ni = (NetworkInterface) netInterfaces.nextElement();
+					// ----------特定情况，可以考虑用ni.getName判断
+					// 遍历所有ip
+					Enumeration<InetAddress> ips = ni.getInetAddresses();
+					while (ips.hasMoreElements()) {
+						ip = (InetAddress) ips.nextElement();
+						if (ip.isSiteLocalAddress() && !ip.isLoopbackAddress() // 127.开头的都是loopback地址
+								&& ip.getHostAddress().indexOf(":") == -1) {
+							bFindIP = true;
+							break;
+						}
+					}
+
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (null != ip) {
+			sIP = ip.getHostAddress();
+		}
+		return sIP;
 	}
 }
